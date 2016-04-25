1c6c1e17cab92208df9b2f0564d27091686f48a5
Djordje Nedeljkovic <djolens@gmail.com>
2016-04-25 19:15:34 -0400

    spring boot

---
+++ src/test/java/com/djordjenedeljkovic/gitrepoanalyser/GitrepoanalyserApplicationTests.java
@@ -0,0 +1,18 @@
+package com.djordjenedeljkovic.gitrepoanalyser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GitrepoanalyserApplication.class)
+@WebAppConfiguration
+public class GitrepoanalyserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
